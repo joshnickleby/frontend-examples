61ad5d51ac3720e7a2ff2ccc1c6305581c8b43da
joshnickleby <josh.nickleby@astontech.com>
2019-06-21 14:26:58 -0500

    Select test

--- src/app/testing/personal/components/character-sheet-dashboard/character-sheet-dashboard.component.spec.ts
+++ src/app/testing/personal/components/character-sheet-dashboard/character-sheet-dashboard.component.spec.ts
@@ -1,16 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CharacterSheetDashboardComponent } from './character-sheet-dashboard.component';
+import {CharacterSheetDashboardComponent} from './character-sheet-dashboard.component';
 import {CharacterSheetService} from '../../services/character-sheet.service';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ListBehaviorSubject} from '../../../../common/list-behavior-subject';
 import {CharacterSheet} from '../../domain/character-sheet.model';
 import {By} from '@angular/platform-browser';
-import {DebugElement} from '@angular/core';
+import {DebugElement, ElementRef} from '@angular/core';
 import {TestListAssertionHelper} from '../../../../common/test-list-assertion.helper';
+import {SingleObjectList} from '../../../../common/single-object-list';
 
-describe('CharacterSheetDashboardComponent', () => {
+fdescribe('CharacterSheetDashboardComponent', () => {
   let component: CharacterSheetDashboardComponent;
   let fixture: ComponentFixture<CharacterSheetDashboardComponent>;
 
@@ -57,6 +57,7 @@ describe('CharacterSheetDashboardComponent', () => {
       providers: [ { provide: CharacterSheetService, useValue: serviceSpy } ]
     })
     .compileComponents().then(() => {
+      // serviceSpy.selectedCharacterSheet$ = new SingleObjectList();
       serviceSpy.characterSheets$ = ListBehaviorSubject.create();
 
       serviceSpy.getAllCharacterSheets.and.callFake(() => serviceSpy.characterSheets$.next(expectedSheets));
@@ -77,5 +78,43 @@ describe('CharacterSheetDashboardComponent', () => {
 
     // check the names match for each row
     sharedSheetLookupFn(sheet => sheet.name, '.sheet-name');
+  }));
+
+  it('show display a character when button gets clicked', async(() => {
+    // assign the service variable of the component
+    component.characterSheetService.selectedCharacterSheet$ = new SingleObjectList();
+
+    // setup the method
+    serviceSpy.getCharacterSheetById.and.callFake(id => {
+      const foundSheet = expectedSheets.find(sheet => sheet.id === id);
+      console.log('------------------------called fake', foundSheet);
+
+      component.characterSheetService.selectedCharacterSheet$.change(foundSheet);
+    });
+
+    // get the first element in the list
+    const rows: DebugElement[] = fixture.debugElement.queryAll(By.css('.sheets'));
+
+    const firstRow: DebugElement = rows[0];
+
+    // get the first button
+    const buttons = firstRow.queryAll(By.css('button'));
+
+    const selectButton = buttons[0];
+
+    // click the button
+    selectButton.nativeElement.click();
+
+    fixture.detectChanges();
+
+    const selectedSheet = component.characterSheetService.selectedCharacterSheet$.getItem();
+
+    // find the selected element id and name sections
+    const idElement = fixture.debugElement.queryAll(By.css('.selected-sheet-id'))[0].nativeElement;
+    const nameElement = fixture.debugElement.queryAll(By.css('.selected-sheet-name'))[0].nativeElement;
+
+    // assert the changes
+    expect(idElement.innerText.includes('' + selectedSheet.id)).toEqual(true);
+    expect(nameElement.innerText.includes(selectedSheet.name)).toEqual(true);
   }));
 });
